1ef86abf3d6e6e188b6d2c1d20f85bef6f5799ea
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-09 12:52:57 -0700

    Add title and tagline styling and header tags

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -15,14 +15,14 @@ function SingleMovie ({movieInFocus, movies}) {
       width:'100vw',
       height:'100vh'
       }}>
-      <p className="single-movie-title"> {correctMovie.title}</p>
-      <p className="single-movie-tagline"> {correctMovie.tagline}</p>
-      <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+      <h3 className="single-movie-title"> 
+        {correctMovie.title}
+        <h4 className="single-movie-tagline"> {correctMovie.tagline}</h4>
+      </h3>
+      {/* <p className="single-movie-tagline"> {correctMovie.tagline}</p> */}
       <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
-      <p className="single-movie-info">Genre:{correctMovie.genres} Revenue:{correctMovie.revenue} Budget:{correctMovie.budget}</p>
-      <p className="single-movie-revenue">Revenue:{correctMovie.revenue}</p>
-      <p className="single-movie-budget">Genre:{correctMovie.budget}</p>
-      <p>Release Date: {correctMovie.release_date}</p>
+      <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+      <p className="single-movie-info">Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}</p>
     </div>
   )
 }
