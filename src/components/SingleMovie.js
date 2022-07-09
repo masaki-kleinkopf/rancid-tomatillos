2706b4a8f224cc8cf23e908d24cd53d5d3cfcd23
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-09 12:44:58 -0700

    Add linear gradient to background image and add basic info

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -7,18 +7,22 @@ function SingleMovie ({movieInFocus, movies}) {
 
   return (
     <div className="SingleMovie" style={{
-      backgroundImage:`url(${correctMovie.backdrop_path})`,
+    backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
+    url(${correctMovie.backdrop_path})`,
       backgroundPosition: 'center',
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
       width:'100vw',
       height:'100vh'
       }}>
-      {/* <img src={correctMovie.backdrop_path} height="300" width="100%"></img> */}
-      <img src={correctMovie.poster_path} height="300" width="200"></img>
-      <p>Title: {correctMovie.title}</p>
-      <p>Rating: {correctMovie.average_rating}</p>
-      <p>Release Date {correctMovie.release_date}</p>
+      <p className="single-movie-title"> {correctMovie.title}</p>
+      <p className="single-movie-tagline"> {correctMovie.tagline}</p>
+      <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+      <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
+      <p className="single-movie-info">Genre:{correctMovie.genres} Revenue:{correctMovie.revenue} Budget:{correctMovie.budget}</p>
+      <p className="single-movie-revenue">Revenue:{correctMovie.revenue}</p>
+      <p className="single-movie-budget">Genre:{correctMovie.budget}</p>
+      <p>Release Date: {correctMovie.release_date}</p>
     </div>
   )
 }
