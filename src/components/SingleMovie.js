d2c0ed2acc4b1abe146abe4337d5353286b8f3db
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-09 11:57:37 -0700

    Add background image and initial styling to single movie view

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,11 +1,20 @@
 import React from "react"
+import "../css/SingleMovie.css"
 
 function SingleMovie ({movieInFocus, movies}) {
 
   const correctMovie = movies.find(movie => movie.id === movieInFocus)
 
   return (
-    <div>
+    <div className="SingleMovie" style={{
+      backgroundImage:`url(${correctMovie.backdrop_path})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width:'100vw',
+      height:'100vh'
+      }}>
+      {/* <img src={correctMovie.backdrop_path} height="300" width="100%"></img> */}
       <img src={correctMovie.poster_path} height="300" width="200"></img>
       <p>Title: {correctMovie.title}</p>
       <p>Rating: {correctMovie.average_rating}</p>
