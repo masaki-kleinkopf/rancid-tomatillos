901bf9094657f7a1820603ac93a577bc8d803953
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-09 16:16:39 -0700

    change h4 to p tag

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -8,7 +8,8 @@ class SingleMovie extends React.Component {
     super(props);
     this.state = {
       id:props.movieInFocus,
-      correctMovie:{}
+      correctMovie:{},
+      error:""
     }
   }
 
@@ -21,34 +22,34 @@ class SingleMovie extends React.Component {
     }
 
   render() {
-    const { correctMovie } = this.state;
+    const { correctMovie, error } = this.state;
 
-    if (!correctMovie) {
+    if (error) {
       return (<p className="error">Uh oh! Something went wrong</p>)
     } else {
-    return (
-      <div className="SingleMovie" style={{
-        backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
-        url(${correctMovie.backdrop_path})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width:'100vw',
-        height:'100vh'
-        }}>
-        <h3 className="single-movie-title"> 
-          {correctMovie.title}
-          <h4 className="single-movie-tagline"> {correctMovie.tagline}</h4>
-        </h3>
-        <div className="single-movie-info-container">
-          <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
-          <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
-          <p className="single-movie-info">
-            Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
-          </p>
+      return (
+        <div className="SingleMovie" style={{
+          backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
+          url(${correctMovie.backdrop_path})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          width:'100vw',
+          height:'100vh'
+          }}>
+          <h3 className="single-movie-title"> 
+            {correctMovie.title}
+            <p className="single-movie-tagline"> {correctMovie.tagline}</p>
+          </h3>
+          <div className="single-movie-info-container">
+            <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
+            <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+            <p className="single-movie-info">
+              Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
+            </p>
+          </div>
         </div>
-      </div>
-    )
+      )
   }
 }
 }
