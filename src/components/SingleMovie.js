10d7642218819eb01d0b99e584d771ef74fa2152
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-09 16:09:57 -0700

    create fetch for SingleMovie

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,33 +1,56 @@
 import React from "react"
 import "../css/SingleMovie.css"
+import getData from "../apiCalls"
 
-function SingleMovie ({movieInFocus, movies}) {
 
-  const correctMovie = movies.find(movie => movie.id === movieInFocus)
+class SingleMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id:props.movieInFocus,
+      correctMovie:{}
+    }
+  }
 
-  return (
-    <div className="SingleMovie" style={{
-      backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
-      url(${correctMovie.backdrop_path})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      width:'100vw',
-      height:'100vh'
-      }}>
-      <h3 className="single-movie-title"> 
-        {correctMovie.title}
-        <h4 className="single-movie-tagline"> {correctMovie.tagline}</h4>
-      </h3>
-      <div className="single-movie-info-container">
-        <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
-        <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
-        <p className="single-movie-info">
-          Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
-        </p>
+  componentDidMount = () => {
+    getData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.id}`)
+      .then(data => {this.setState({correctMovie: data.movie})})
+      .catch(error => {
+        this.setState({error:error.message})
+      })
+    }
+
+  render() {
+    const { correctMovie } = this.state;
+
+    if (!correctMovie) {
+      return (<p className="error">Uh oh! Something went wrong</p>)
+    } else {
+    return (
+      <div className="SingleMovie" style={{
+        backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
+        url(${correctMovie.backdrop_path})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width:'100vw',
+        height:'100vh'
+        }}>
+        <h3 className="single-movie-title"> 
+          {correctMovie.title}
+          <h4 className="single-movie-tagline"> {correctMovie.tagline}</h4>
+        </h3>
+        <div className="single-movie-info-container">
+          <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
+          <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+          <p className="single-movie-info">
+            Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+}
 }
 
 export default SingleMovie;
