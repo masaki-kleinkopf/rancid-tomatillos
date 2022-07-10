bbe643a304b77c4272a9a2e03f7873140f51dfec
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-10 11:00:09 -0700

    Remove commented code

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -42,43 +42,12 @@ class SingleMovie extends React.Component {
           <div className="single-movie-info-container">
             <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
             <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
-            <p className="single-movie-info">
+            <div className="single-movie-info">
               Release Date: {correctMovie.release_date} {!!correctMovie.genres && <p>Genre: {correctMovie.genres.join(", ")}</p>} {!!correctMovie.revenue && <p>Revenue: ${correctMovie.revenue}</p>} {!!correctMovie.budget  && <p>Budget: ${correctMovie.budget}</p>}
-            </p>
+            </div>
           </div>
         </div>
-        
-      
-   
-
-  //   if (error) {
-  //     return (<p className="error">Uh oh! Something went wrong</p>)
-  //   } else {
-  //     return (
-  //       <div className="SingleMovie" style={{
-  //         backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
-  //         url(${correctMovie.backdrop_path})`,
-  //         backgroundPosition: 'center',
-  //         backgroundSize: 'cover',
-  //         backgroundRepeat: 'no-repeat',
-  //         width:'100vw',
-  //         height:'100vh'
-  //         }}>
-  //         <h3 className="single-movie-title"> 
-  //           {correctMovie.title}
-  //           <p className="single-movie-tagline"> {correctMovie.tagline}</p>
-  //         </h3>
-  //         <div className="single-movie-info-container">
-  //           <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
-  //           <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
-  //           <p className="single-movie-info">
-  //             Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
-  //           </p>
-  //         </div>
-  //       </div>
-  //     )
-  // }
-}
+  }
 }
 
 
