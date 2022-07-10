7ca309d25fc9238c2110d68f90eca21f436f77a8
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-10 10:11:59 -0700

    Add ternary for conditional rendering

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -22,13 +22,11 @@ class SingleMovie extends React.Component {
     }
 
   render() {
+    
     const { correctMovie, error } = this.state;
 
-    if (error) {
-      return (<p className="error">Uh oh! Something went wrong</p>)
-    } else {
-      return (
-        <div className="SingleMovie" style={{
+    return error ? <p className="error">Uh oh! Something went wrong</p>: 
+      <div className="SingleMovie" style={{
           backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
           url(${correctMovie.backdrop_path})`,
           backgroundPosition: 'center',
@@ -49,8 +47,36 @@ class SingleMovie extends React.Component {
             </p>
           </div>
         </div>
-      )
-  }
+      
+   
+
+  //   if (error) {
+  //     return (<p className="error">Uh oh! Something went wrong</p>)
+  //   } else {
+  //     return (
+  //       <div className="SingleMovie" style={{
+  //         backgroundImage:`linear-gradient(to bottom, transparent 0%, black 60%),
+  //         url(${correctMovie.backdrop_path})`,
+  //         backgroundPosition: 'center',
+  //         backgroundSize: 'cover',
+  //         backgroundRepeat: 'no-repeat',
+  //         width:'100vw',
+  //         height:'100vh'
+  //         }}>
+  //         <h3 className="single-movie-title"> 
+  //           {correctMovie.title}
+  //           <p className="single-movie-tagline"> {correctMovie.tagline}</p>
+  //         </h3>
+  //         <div className="single-movie-info-container">
+  //           <p className="single-movie-overview">Overview:<br></br>{correctMovie.overview}</p>
+  //           <p>Average User Rating: {Math.round(correctMovie.average_rating)}</p>
+  //           <p className="single-movie-info">
+  //             Release Date: {correctMovie.release_date} Genre:{correctMovie.genres} Revenue: ${correctMovie.revenue} Budget: ${correctMovie.budget}
+  //           </p>
+  //         </div>
+  //       </div>
+  //     )
+  // }
 }
 }
 
